4cde3c2ee34f5c49629c6a7ea2c3282d62446d13
moosefriend <moosefriend@t-online.de>
2024-08-11 16:57:13 +0200

    Added injection of name lists into ksp.tmGrammar.json

--- vscode_extension/bin/yaml2json.py
+++ vscode_extension/bin/yaml2json.py
@@ -16,12 +16,15 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 ##############################################################################
+"""
+Convert the yaml files to json file required for the VS Code Language Extension
+"""
 import json
 from pathlib import Path
 import yaml
 from yaml import SafeLoader
 
-cur_dir = Path(__file__).parent
+root_dir = Path(__file__).parent.parent
 input_file_list = [
     Path("language-configuration.yml"),
     Path("syntaxes/ksp.tmGrammar.yml"),
@@ -33,8 +36,8 @@ for input_file in input_file_list:
     base_name = input_file.stem
     output_file = output_dir / f"{base_name}.json"
     print(f"Converting {input_file.as_posix()} -> {output_file.as_posix()}", end="")
-    input_path = cur_dir / input_file
-    output_path = cur_dir / output_file
+    input_path = root_dir / input_file
+    output_path = root_dir / output_file
     output_path.parent.mkdir(parents=True, exist_ok=True)
     if input_path.is_file():
         with input_path.open("r") as f:
