a55094cbe0201e28a23f07207fdd82fb73ea8ed6
moosefriend <moosefriend@t-online.de>
2024-09-23 19:08:31 +0200

    Refactoring to simplify (not yet finished)

--- doc_parser/bin/pdf2txt.py
+++ doc_parser/bin/pdf2txt.py
@@ -16,13 +16,35 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 ##############################################################################
+import argparse
+import logging
 from pathlib import Path
 
 from ksp_base.base_main_parser import BaseMainParser
+from ksp_base.constants import ParserType
+
+log = logging.getLogger(__name__)
 
 
-class KspMainParser(BaseMainParser):
-    """Page ksp_parser for Kontakt 7.6 KSP reference manual"""
+parser = argparse.ArgumentParser()
+parser.add_argument('-v', '--version', required=True, help="Kontakt KSP version")
+parser.add_argument('-p', '--pdf', required=True, help="Kontakt KSP reference manual PDF input file for parsing")
+parser.add_argument('-c', '--convert', help="Convert the PDF file into a text file", type=bool, default=True)
+parser.add_argument('-l', '--logfile', help="Log file", default=None)
+args = parser.parse_args()
 
-    def __init__(self, version: str, pdf_file: Path, out_dir: Path, delimiter: str):
-        super().__init__(version, pdf_file, out_dir, delimiter, page_offset=8, page_header_lines=3)
+logging.basicConfig(
+    filename=args.logfile,
+    level=logging.INFO,
+    format="[%(asctime)s] %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S"
+)
+root = Path(__file__).parent.parent
+out_dir = root / "out"
+version = args.version
+pdf_file = args.pdf
+parser = BaseMainParser.get_parser(ParserType.MAIN, version, pdf_file, out_dir, ",")
+if args.convert:
+    parser.convert_to_text()
+else:
+    parser.parse()
