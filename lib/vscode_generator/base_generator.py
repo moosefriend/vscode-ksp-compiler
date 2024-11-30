190c71e1e9c59558b1ce5f5a48db5a7d1070a878
moosefriend <moosefriend@t-online.de>
2024-11-30 21:15:32 +0100

    Started to implement the type script generators

--- lib/vscode_generator/base_generator.py
+++ lib/vscode_generator/base_generator.py
@@ -17,32 +17,33 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 ##############################################################################
 import logging
+from abc import abstractmethod
 
 from config.constants import ItemType
-from config.system_config import SystemConfig
 from doc_item.doc_item_reader import DocItemReader
-from util.file_util import replace_in_file
-from vscode_generator.base_generator import BaseGenerator
 
 log = logging.getLogger(__name__)
 
 
-class GrammarGenerator(BaseGenerator):
+class BaseGenerator:
     @staticmethod
+    def read_name_list(item_type: ItemType) -> list[str]:
+        """
+        From the *.csv file read the Name column and combine the names with "|".
+
+        :param item_type: ItemType for get the file to read
+        """
+        name_list = []
+        with DocItemReader(item_type) as csv_reader:
+            log.info(f"Read name list from {csv_reader.csv_file.as_posix()}")
+            for doc_item in csv_reader:
+                name_list.append(doc_item.name)
+        return name_list
+
+    @staticmethod
+    @abstractmethod
     def process():
         """
-        Reads the grammar JSON file and replace <<category>> with the name list.
+        Generate the corresponding objects.
+        This method must be overridden.
         """
-        replace_list = [
-            ItemType.CALLBACK,
-            ItemType.COMMAND,
-            ItemType.FUNCTION,
-            ItemType.WIDGET
-        ]
-        log.info(f"Modify {SystemConfig().grammar_json.as_posix()}")
-        for item_type in replace_list:
-            search_string = f"<<{item_type.category()}>>"
-            name_list = BaseGenerator.read_name_list(item_type)
-            replace_string = "|".join(name_list)
-            replace_in_file(SystemConfig().grammar_json, search_string, replace_string)
-
