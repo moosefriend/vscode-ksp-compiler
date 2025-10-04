866bcd35800fd70f033debf95e526e7b042ff145
moosefriend <moosefriend@t-online.de>
2025-10-04 18:08:45 +0200

    Monkey patched ksp_compiler.ParseException to save the plain error message

--- vscode_extension/bin/ksp_compiler_wrapper.py
+++ vscode_extension/bin/ksp_compiler_wrapper.py
@@ -16,24 +16,38 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 ##############################################################################
-import re
 import traceback
 import sys
 
 import _find_ksp_compiler  # noqa
-from ksp_compiler import main, ParseException
+from ksp_compiler import main, ParseException as OriginalParseException
+import ksp_compiler
+
+class PatchedParseException(OriginalParseException):
+    """Patch the ParseException to save the clean message"""
+
+    def __init__(self, line, message):
+        """
+        Patch the ParseException to save the clean message.
+
+        :param line: Line object or None
+        :param message: Error message
+        """
+        super().__init__(line, message)
+        self.error_message = message.strip()
 
 
 if __name__ == "__main__":
     exit_code = 0
     try:
+        # Monkey patch the ParseException in ksp_compiler
+        # This is needed to get the clean error message
+        ksp_compiler.ParseException = PatchedParseException
         # Call the main function from ksp_compiler
         main()
-    except ParseException as ex:
+    except PatchedParseException as ex:
         print(">>> BEGIN Error", file=sys.stderr)
-        message = ex.message
-        message = message.split("\n\n", 1)[0].strip()  # Keep only the first paragraph
-        message = re.sub(r" \(line \d+\)$", "", message)
+        message = ex.error_message
         print(message, file=sys.stderr)
         if ex.line:
             print(f">>> Command: {ex.line.command}", file=sys.stderr)
